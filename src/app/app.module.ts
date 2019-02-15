401a31b9e33d7015b2167d22a67c80fc60d038fc
Anudeep Challa <anudeep.challa@ggktech.com>
2019-02-15 18:04:53 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ChatModule } from './chat/chat.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { ChatComponent } from './chat/chat.component';
+// import { DialogUserComponent } from './dialog-user/dialog-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ChatComponent,
+    // DialogUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ChatModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
